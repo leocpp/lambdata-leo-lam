e951a0b8328c6a6cc90cc1ee9c9b35fb81d851a8
leocpp <leocpp@users.noreply.github.com>
2019-09-03 20:45:30 -0700

    Added twine and __init__.py and setup.py files

---
+++ __init__.py
@@ -0,0 +1,9 @@
+"""
+lambdata-leo-lam - A collection of data science helper functions.
+"""
+
+import pandas as pd
+import numpy as np
+
+ZEROES = np.zeros(5)
+ONES = np.ones(10)
---
+++ setup.py
@@ -0,0 +1,31 @@
+"""
+lambdata-leo-lam - A Collection of Data Science helper functions.
+"""
+
+import setuptools
+
+REQUIRED = [
+    "numpy",
+    "pandas"
+]
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name="lambdata-leo-lam",
+    version="0.0.1",
+    author="leo-lam",
+    description="A collection of Data Science helper functions",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/leocpp/lambdata-leo-lam",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_required=REQUIRED,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
